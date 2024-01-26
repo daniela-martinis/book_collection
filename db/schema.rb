0e481eca2e0708a850b703f635dac8066d9f38e1
danielamartinis <daniela.a.martinis@gmail.com>
2024-01-26 11:33:03 -0600

    Added price field for user input. Made price column in book table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_25_235414) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_26_172229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_25_235414) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+    t.decimal "price"
   end
 
 end
