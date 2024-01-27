17871a9cb8995d1dc1f91afe4e7e5a428f72ccf9
danielamartinis <daniela.a.martinis@gmail.com>
2024-01-27 17:55:42 -0600

    Added delete page to confirm whether user wants to delete the book.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :books
+  root "books#index"
+  resources :books do
+    get 'delete', on: :member
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
   # root "articles#index"
-
 end
